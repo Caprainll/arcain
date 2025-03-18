dc6de44554678f758011dcf5597d9c6987b786cb
mmyca <mycamaymay15@gmail.com>
2025-03-18 17:54:49 +0800

    added login and registration design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">AARON </h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">CAPILITAN </h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                   
@@ -68,7 +68,7 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">GWAPO</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">RANNIEL</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                
